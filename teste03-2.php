50c2ffb53a57c519fcccf9353f8297a1aefa9fd9
evertonmessias <everton.messias@gmail.com>
2019-12-16 17:21:54 -0300

    16-12-2019_17:21:54

---
+++ teste03-2.php
@@ -0,0 +1,12 @@
+<?php
+function imc($p,$a){
+	return number_format(($p/pow($a, 2)),2);
+}
+$resposta = imc(@$_POST['peso'],@$_POST['altura']);
+echo "<p>IMC = $resposta</p><br>";
+if($resposta < 20) {echo "<p>ABAIXO DO PESO</p>";}
+elseif($resposta >= 20 && $resposta < 25) {echo "<p>PESO NORMAL</p>";}
+else {echo "<p>OBESIDADE</p>";}
+
+
+?>
--- teste03.php
+++ teste03.php
@@ -4,57 +4,41 @@
 <meta charset="UTF-8"/>
 <style type="text/css">
 body{background-color: #ccc;}
-fieldset{background-color:#fff;display: block;position: relative;margin: 0 auto;width: 250px;height:250px;text-align: center;box-shadow: 5px 5px #808080;}
+fieldset,#resp{display: block;position: relative;margin: 0 auto;}
+fieldset{background-color:#fff;width: 250px;height:260px;text-align: center;box-shadow: 5px 5px #808080;}
 legend{background-color: #808080;color: white;width: 80px;}
-input{width: 100px;}label{font-weight: bolder;}
-input.botao{background-color: #808080;color: white;font-weight: bold;}
+input{width: 88px;}label{font-weight: bolder;}
+input.campo{width: 180px;}
+#botao1,#botao2{background-color: #808080;color: white;font-weight: bold;}
+#resp{text-align: center;}
 </style>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+<script type="text/javascript" src="./jquery.js"></script>
 <script type="text/javascript" >
-function apagar() {
-	$("#imc,#cond").html("&nbsp;");
-}
+$(function(){
+	var form = $("formulario");
+	var peso = form.peso.val;
+	var altura = form.altura.val;
+	$("botao1").click(function(){
+	if(peso > 20 && peso < 280 && altura > 1 && altura < 2.5){		
+		console.log(peso);console.log(altura);
+	$.post("./teste03-2.php", {peso: peso, altura: altura}, function(mostrar){
+            $('#resp').html(mostrar);})
+	}else{$('#resp').html("<p>Dados incorretos<p>");} 
+	});
+	$("botao2").click(function(){
+		
+	});	
+})
 </script>
-
 </head>
 <body>
-<form method="GET">
-<fieldset><legend><h3>ICM</h3></legend>
-<br>
-<label>Peso&ensp;&nbsp;: <input type="number" name="peso" min="0" max="200" step="0.1" required placeholder="Seu Peso" /></label><br><br>
-<label>Altura: <input type="number" name="altura" min="0" max="2.2" step="0.1" required placeholder="Sua Altura" /></label><br><br>
-<input class="botao" name="botao" type="submit" value="CALCULAR">&emsp;<input class="botao" type="reset" value="APAGAR" onclick="apagar();">
-
-<?php
-
-function imc($p,$a){
-	return number_format(($p/pow($a, 2)),2);
-}
-
-if(isset($_GET['botao'])) {
-$resposta = imc(@$_GET['peso'],@$_GET['altura']);
-
-echo "<p id='imc'>IMC = $resposta</p>";
-if($resposta < 20) {echo "<p id='cond'>ABAIXO DO PESO</p>";}
-elseif($resposta >= 20 && $resposta < 25) {echo "<p id='cond'>PESO NORMAL</p>";}
-else {echo "<p id='cond'>OBESIDADE</p>";}}
-
-?>
-
-<!-- function imc($p,$a){
-	return number_format(($p/pow($a, 2)),2);
-}
-
-if(isset($_GET['botao'])) {
-$resposta = imc(@$_GET['peso'],@$_GET['altura']);
-
-echo "<p id='imc'>IMC = $resposta</p>";
-if($resposta < 20) {echo "<p id='cond'>ABAIXO DO PESO</p>";}
-elseif($resposta >= 20 && $resposta < 25) {echo "<p id='cond'>PESO NORMAL</p>";}
-else {echo "<p id='cond'>OBESIDADE</p>";}} -->
-
+<form id="formulario" method="POST">
+<fieldset><legend><h3>ICM</h3></legend><br>
+<label><input class="campo" type="number" name="peso" step="0.1" placeholder="Digite Seu Peso" /></label><br><br>
+<label><input class="campo" type="number" name="altura" step="0.1" placeholder="Digite Sua Altura" /></label><br><br>
+<input id="botao1" type="button" value="CALCULAR">&nbsp;
+<input id="botao2" type="reset" value="APAGAR"><br><br>
+<div id="resp">Resposta</div>
 </fieldset>
 </form>
 
