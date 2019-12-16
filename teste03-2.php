fc5c36fc4b82b5726af2d7988959516d009b93de
evertonmessias <everton.messias@gmail.com>
2019-12-16 19:59:44 -0300

    16-12-2019_19:59:44

--- teste03-2.php
+++ teste03-2.php
@@ -3,7 +3,7 @@ function imc($p,$a){
 	return number_format(($p/pow($a, 2)),2);
 }
 $resposta = imc(@$_POST['peso'],@$_POST['altura']);
-echo "<p>IMC = $resposta</p><br>";
+echo "<p>IMC = $resposta</p>";
 if($resposta < 20) {echo "<p>ABAIXO DO PESO</p>";}
 elseif($resposta >= 20 && $resposta < 25) {echo "<p>PESO NORMAL</p>";}
 else {echo "<p>OBESIDADE</p>";}
--- teste03.php
+++ teste03.php
@@ -1,46 +1,102 @@
 <html>
+
 <head>
-<title>IMC</title>
-<meta charset="UTF-8"/>
-<style type="text/css">
-body{background-color: #ccc;}
-fieldset,#resp{display: block;position: relative;margin: 0 auto;}
-fieldset{background-color:#fff;width: 250px;height:260px;text-align: center;box-shadow: 5px 5px #808080;}
-legend{background-color: #808080;color: white;width: 80px;}
-input{width: 88px;}label{font-weight: bolder;}
-input.campo{width: 180px;}
-#botao1,#botao2{background-color: #808080;color: white;font-weight: bold;}
-#resp{text-align: center;}
-</style>
-<script type="text/javascript" src="./jquery.js"></script>
-<script type="text/javascript" >
-$(function(){
-	var form = $("formulario");
-	var peso = form.peso.val;
-	var altura = form.altura.val;
-	$("botao1").click(function(){
-	if(peso > 20 && peso < 280 && altura > 1 && altura < 2.5){		
-		console.log(peso);console.log(altura);
-	$.post("./teste03-2.php", {peso: peso, altura: altura}, function(mostrar){
-            $('#resp').html(mostrar);})
-	}else{$('#resp').html("<p>Dados incorretos<p>");} 
-	});
-	$("botao2").click(function(){
-		
-	});	
-})
-</script>
+	<title>IMC</title>
+	<meta charset="UTF-8" />
+	<style type="text/css">
+		body {
+			background-color: #ccc;
+		}
+
+		fieldset,
+		#resp {
+			display: block;
+			position: relative;
+			margin: 0 auto;
+		}
+
+		fieldset {
+			background-color: #fff;
+			width: 250px;
+			height: 280px;
+			text-align: center;
+			box-shadow: 5px 5px #808080;
+		}
+
+		legend {
+			background-color: #808080;
+			color: white;
+			width: 80px;
+		}
+
+		input {
+			width: 88px;
+		}
+
+		label {
+			font-weight: bolder;
+		}
+
+		input#peso,
+		input#altura {
+			width: 180px;
+		}
+
+		#botao1,
+		#botao2 {				
+			background-color: #808080;
+			color: white;
+			font-weight: bold;
+		}
+
+		#resp {
+			text-align: center;
+		}
+	</style>
+	<script type="text/javascript" src="./jquery.js"></script>
+	<script type="text/javascript">
+		$(function() {
+			$("#botao1").click(function() {
+				var peso = $("#peso").val();
+				var altura = $("#altura").val();
+				if ((peso > 50 && peso < 250) && (altura >= 1 && altura <= 2.5)) {
+					console.log(peso);
+					console.log(altura);
+					$.post("./teste03-2.php", {
+						peso: peso,
+						altura: altura
+					}, function(mostrar) {
+						$('#resp').html(mostrar)
+						.css({"color":"black","font-weight":"normal"});
+					})
+				} else {
+					$('#resp').html("<p>Dados incorretos<p>")
+					.css({"color":"red","font-weight":"bold"});
+				}
+			});
+			$("#botao2").click(function() {
+				$('#resp').html("<p>Resposta<p>");
+			});
+		})
+	</script>
 </head>
+
 <body>
-<form id="formulario" method="POST">
-<fieldset><legend><h3>ICM</h3></legend><br>
-<label><input class="campo" type="number" name="peso" step="0.1" placeholder="Digite Seu Peso" /></label><br><br>
-<label><input class="campo" type="number" name="altura" step="0.1" placeholder="Digite Sua Altura" /></label><br><br>
-<input id="botao1" type="button" value="CALCULAR">&nbsp;
-<input id="botao2" type="reset" value="APAGAR"><br><br>
-<div id="resp">Resposta</div>
-</fieldset>
-</form>
+	<form method="POST">
+		<fieldset>
+			<legend>
+				<h3>ICM</h3>
+			</legend><br>
+			<label><input id="peso" type="number" step="0.1" placeholder="Digite Seu Peso (Kg)" /></label><br><br>
+			<label><input id="altura" type="number" step="0.1" placeholder="Digite Sua Altura (m)" /></label><br><br>
+			<input id="botao1" type="button" value="CALCULAR">&nbsp;
+			<input id="botao2" type="reset" value="APAGAR"><br><br>
+			<div id="resp">
+				<p>Resposta</p>
+			</div>
+		</fieldset>
+	</form>
 
 </body>
+
 </html>
