2e74ded13c4f75e24fed723b4d662684d8a23dd2
evertonmessias <everton.messias@gmail.com>
2020-01-16 20:21:25 -0300

    16-01-2020_20:21:25

--- teste.php
+++ teste.php
@@ -6,9 +6,18 @@ echo "<h1>10:00:00 - quinta 16 janeiro 2020</h1>";
 echo "<h1>1579179600</h1>";
 
 $timestamp = 1579179600;
-$formato = 'Y-m-d H:i:s';
+$formato = 'd-m-Y H:i:s';
 
 $data = new DateTime('NOW', new DateTimeZone('America/Recife'));
 $data->setTimestamp($timestamp);
 echo $data->format($formato);
+
+echo "<br>";echo "<br>";
+
+$futruro = new DateTime('NOW', new DateTimeZone('America/Recife'));
+$futruro->add(new DateInterval('P10D')); // add 10 dias
+echo $futruro->format($formato);
+echo "<br>";
+echo $futruro->getTimestamp();
+
 ?>
