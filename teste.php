bafaf8110c033b10f2da2bce88c204b44bd3d49c
evertonmessias <everton.messias@gmail.com>
2020-01-16 20:53:15 -0300

    16-01-2020_20:53:15

--- teste.php
+++ teste.php
@@ -1,23 +1,30 @@
 <?php
 
-// curl -o temp.html http://portfolio.em/testes/php/teste.php
+//Obs.: curl -o temp.html http://portfolio.em/testes/php/teste.php
+
+if(@$_GET['dia'] && @$_GET['mes'] && @$_GET['ano']){
+    $hoje = @$_GET['ano']."-".@$_GET['mes']."-".@$_GET['dia'];    
+}else{$hoje = 'now';}
+
+$formato = 'd/m/Y H:i:s';
+
 
 echo "<h1>10:00:00 - quinta 16 janeiro 2020</h1>";
 echo "<h1>1579179600</h1>";
-
 $timestamp = 1579179600;
 $formato = 'd-m-Y H:i:s';
-
 $data = new DateTime('NOW', new DateTimeZone('America/Recife'));
 $data->setTimestamp($timestamp);
 echo $data->format($formato);
 
-echo "<br>";echo "<br>";
+echo "<br>";echo "<hr>";echo "<br>";
 
-$futruro = new DateTime('NOW', new DateTimeZone('America/Recife'));
+$futruro = new DateTime($hoje, new DateTimeZone('America/Recife'));
+echo $futruro->format($formato);
+echo "<br>";
+echo $futruro->getTimestamp();
+echo "<br>";
 $futruro->add(new DateInterval('P10D')); // add 10 dias
 echo $futruro->format($formato);
 echo "<br>";
 echo $futruro->getTimestamp();
-
-?>
