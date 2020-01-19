befb6049127274a6c961021b0c2a0bc000a5dadf
evertonmessias <everton.messias@gmail.com>
2020-01-19 20:17:09 -0300

    19-01-2020_20:17:09

--- teste.php
+++ teste.php
@@ -1,5 +1,7 @@
 <?php
 
+// ANOTAÇÕES
+
 //Obs.: curl -o temp.html http://portfolio.em/testes/php/teste.php
 
 if(@$_GET['dia'] && @$_GET['mes'] && @$_GET['ano']){
@@ -7,7 +9,6 @@ if(@$_GET['dia'] && @$_GET['mes'] && @$_GET['ano']){
 }else{$hoje = 'now';}
 
 $formato = 'd/m/Y H:i:s';
-
 
 echo "<h1>10:00:00 - quinta 16 janeiro 2020</h1>";
 echo "<h1>1579179600</h1>";
@@ -28,3 +29,30 @@ $futuro->add(new DateInterval('P10D')); // add 10 dias
 echo $futuro->format($formato);
 echo "<br>";
 echo $futuro->getTimestamp();
+
+// *************************************************************
+
+$arq = @$_GET['arq'];
+
+function lerArquivo($pathArquivo){
+    $html = fopen($pathArquivo,'r');
+    $conteudo = '';
+    while(!feof($html)) {
+        $conteudo = $conteudo . fgets($html);
+    }
+    fclose($html);
+    return $conteudo;
+}
+
+$msg = lerArquivo($arq);
+
+$cabecalho = 'MIME-Version: 1.0' . "\r\n";
+$cabecalho .= 'Content-type: text/html; charset=iso-8859-1;' . "\r\n";
+
+$destino = 'everton.messias@gmail.com';
+$assunto = 'RESERVAS IC - Tarefas da Semana';
+
+$enviar = mail($destino, $assunto, $msg, $cabecalho);
+
+if($enviar){echo "<h1 style='text-align: center;text-decoration: underline;'>CONTEUDO ENVIADO:</h1>".$msg;}
+else{echo "Erro - Mensagem não Enviada !";}
