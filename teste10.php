4bc7a8c46c77787af9139e035b3941d8258ab773
evertonmessias <everton.messias@gmail.com>
2020-03-02 17:29:41 -0300

    02-03-2020_17:29:41

--- teste10.php
+++ teste10.php
@@ -8,34 +8,45 @@ interface Banco
     public function saldo($nome);
 }
 
+final class Cliente
+{
+    public $nome;
+    public $cpf;
+    public $email;
+
+    public function __construct($nome, $cpf, $email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("<p>E-mail inválido</p>");
+        } elseif (!$nome || !$cpf) {
+            throw new Exception("<p>Dados incorretos</p>");
+        } else {
+            $this->nome = $nome;
+            $this->cpf = $cpf;
+            $this->email = $email;
+        }
+    }
+}
+
 //******************************************************************* */
 
 abstract class ContaBanco implements Banco
 {     // abstract NÃO PODE ser instanciada    	
     protected $agencia;
     protected $conta;
-    protected $saldo;
-    protected $email;
+    protected $saldo; 
+    public $cliente;   
 
-    public static function getBanco()
+    public static function getDados()
     { // static não precisa ser instanciada
-        return self::banco;
+        echo self::banco;
     }
 
-    protected function __construct($agencia, $conta, $saldo, $email)
+    protected function __construct($agencia, $conta, $saldo)
     {
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            throw new Exception("<p>E-mail inválido</p>");
-        }elseif($saldo == 0){
-            throw new Exception("<p>Saldo Nulo</p>");
-        }elseif(!$agencia || !$conta){
-            throw new Exception("<p>Dados incorretos</p>");
-        }else{
-            $this->agencia = $agencia;
-            $this->conta = $conta;
-            $this->saldo = $saldo;
-            $this->email = $email;
-        }        
+        $this->agencia = $agencia;
+        $this->conta = $conta;
+        $this->saldo = $saldo;            
     }
     public function depositar($valor)
     {
@@ -63,12 +74,12 @@ final class Corrente extends ContaBanco
 { // final não pode ser extendida 
     const tipo = "Conta Corrente";
     private $limite;
-    public function __construct($agencia, $conta, $saldo, $email, $limite)
+    public function __construct($agencia, $conta, $saldo, $limite)
     {
-        parent::__construct($agencia, $conta, $saldo, $email); //herda algumas características e
+        parent::__construct($agencia, $conta, $saldo); //herda algumas características e
         $this->limite = $limite; // implementa uma nova ( POLIMORFISMO )
-        echo "<p>OK, <b>Conta Corrente</b> {$this->conta}, <b>AG</b> {$this->agencia}, Criada no <b>" . 
-        parent::banco . "</b> para: <u>{$this->email}</u> , <b>saldo Atual</b>: {$this->saldo}</p>";
+        echo "<p>OK, <b>Conta Corrente</b> {$this->conta}, <b>AG</b> {$this->agencia}, Criada no <b>" .
+            parent::banco . "</b> para: <u>{$this->cliente}</u> , <b>saldo Atual</b>: {$this->saldo}</p>";
     }
     public function sacar($valor)
     { //( POLIMORFISMO )
@@ -81,7 +92,7 @@ final class Corrente extends ContaBanco
     }
     public function saldo($nome)
     {
-        echo "<p>O saldo atual na " . self::tipo . " ({$nome }) é {$this->saldo}</p>";
+        echo "<p>O saldo atual na " . self::tipo . " ({$nome}) é {$this->saldo}</p>";
     }
 }
 
@@ -90,56 +101,59 @@ final class Corrente extends ContaBanco
 final class Poupanca extends ContaBanco
 {
     const tipo = "Conta Poupança";
-    public function __construct($agencia, $conta, $saldo, $email)
+    public function __construct($agencia, $conta, $saldo)
     {
-        parent::__construct($agencia, $conta, $saldo, $email);
-        echo "<p>OK, <b>Conta Poupança</b> {$this->conta}, <b>AG</b> {$this->agencia}, Criada no <b>" . 
-        parent::banco . "</b> para: <u>{$this->email}</u> , <b>saldo Atual</b>: {$this->saldo}</p>";
+        parent::__construct($agencia, $conta, $saldo);
+        echo "<p>OK, <b>Conta Poupança</b> {$this->conta}, <b>AG</b> {$this->agencia}, Criada no <b>" .
+            parent::banco . "</b> para: <u>{$this->cliente}</u> , <b>saldo Atual</b>: {$this->saldo}</p>";
     }
     public function saldo($nome)
     {
-        echo "<p>O saldo atual na " . self::tipo . " ({$nome }) é {$this->saldo}</p>";
+        echo "<p>O saldo atual na " . self::tipo . " ({$nome}) é {$this->saldo}</p>";
     }
 }
 
 //******************************************************************* */
 
-echo "<h2><u>Bem vindo ao " . ContaBanco::getBanco() . "</u></h2>";
-$ok1 = $ok2 = false;
+echo "<h2><u>Bem vindo ao " . ContaBanco::getDados() . "</u></h2>";
+$ok = false;
 
-try{
-    $c1 = new Corrente('a233', '24xxx', 10000, "email_1_@asdf.com", 500);
-    $ok1 = true;
-}catch(Exception $e){
-    echo $e->getMessage();
-}
-try{
-    $p1 = new Poupanca('a233', '24xxx', 10000, "email_2_@asdf.com");
-    $ok2 = true;
-}catch(Exception $e){
+try {
+    $cl = new Cliente("Fulano","123456789-00","email@asdf.com");
+    $ok = true;
+} catch (Exception $e) {
     echo $e->getMessage();
 }
 
-if($ok1 && $ok2){
-echo "<h3>Movimentos na CC (c1):</h3>";
-$c1->depositar(5000);
-$c1->sacar(9000);
-$c1->sacar(9000);
+if ($ok) {
 
-echo "<h3>Movimentos na CP (p1):</h3>";
-$p1->depositar(8000);
-$p1->sacar(9000);
-$p1->sacar(9000);
+    $c1 = new Corrente('a233', '24xxx', 10000, 1000);
+    $p1 = new Poupanca('a233', '24xxx', 10000);
 
-echo "<h3>Clones:</h3>";
-$p2 = clone $p1;
-$c2 = clone $c1;
+    $c1->cliente = $cl;
+    $p1->cliente = $cl;
 
-$p2->depositar(5000);
-$c2->depositar(5000);
+    echo $c1->cliente->nome;
 
-$p1->saldo('p1');
-$p2->saldo('p2');
-$c1->saldo('c1');
-$c2->saldo('c2');
+    echo "<h3>Movimentos na CC (c1):</h3>";
+    $c1->depositar(5000);
+    $c1->sacar(9000);
+    $c1->sacar(9000);
+
+    echo "<h3>Movimentos na CP (p1):</h3>";
+    $p1->depositar(8000);
+    $p1->sacar(9000);
+    $p1->sacar(9000);
+
+    echo "<h3>Clones:</h3>";
+    $p2 = clone $p1;
+    $c2 = clone $c1;
+
+    $p2->depositar(5000);
+    $c2->depositar(5000);
+
+    $p1->saldo('p1');
+    $p2->saldo('p2');
+    $c1->saldo('c1');
+    $c2->saldo('c2');
 }
