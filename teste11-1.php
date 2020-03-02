ef68ed02fae0f4936f609ae96ef99c816f93c2f6
evertonmessias <everton.messias@gmail.com>
2020-03-01 22:36:21 -0300

    01-03-2020_22:36:21

--- teste11-1.php
+++ teste11-1.php
@@ -1,31 +1,47 @@
 <?php
 
-class Eq2grau {
-    
-    private $valorA, $valorB, $valorC;
-            
-    function __construct($valorA, $valorB, $valorC) {
+final class Eq2grau
+{
+
+    private $valorA;
+    private $valorB;
+    private $valorC;
+
+    function __construct($valorA, $valorB, $valorC)
+    {
         $this->valorA = $valorA;
         $this->valorB = $valorB;
-        $this->valorC = $valorC;    }
-    
-    function escrever(){ // string da equação
-        if($this->valorB < 0 && $this->valorC < 0){
-        $eq = $this->valorA."x<sup>2</sup>&nbsp;".$this->valorB."x&nbsp;".$this->valorC."&nbsp;=&nbsp;0";}
-        elseif($this->valorB >= 0 && $this->valorC < 0){
-        $eq = $this->valorA."x<sup>2</sup>&nbsp;+".$this->valorB."x&nbsp;".$this->valorC."&nbsp;=&nbsp;0";}
-        elseif($this->valorB < 0 && $this->valorC >= 0){
-        $eq = $this->valorA."x<sup>2</sup>&nbsp;".$this->valorB."x&nbsp;+".$this->valorC."&nbsp;=&nbsp;0";}
-        else{
-        $eq = $this->valorA."x<sup>2</sup>&nbsp;+".$this->valorB."x&nbsp;+".$this->valorC."&nbsp;=&nbsp;0";}
-        return $eq; }
-        
-    function calcular(){ // cálculo da equação        
-        if($this->valorA == 0) {return ["Erro: 'A' não pode ser Zero!",'&empty;','&empty;','&empty;'];}
-        else {$d=((pow($this->valorB, 2))-(4*$this->valorA*$this->valorC)); // cálculo do delta               
-        if ($d < 0){return [$this->escrever(),number_format($d,1),'&empty;','&empty;'];}
-        else{$x1=((-1*$this->valorB)+(sqrt($d)))/(2*$this->valorA); // cálculo de Basckara
-             $x2=((-1*$this->valorB)-(sqrt($d)))/(2*$this->valorA);
-        return [$this->escrever(),number_format($d,1),number_format($x1,1),number_format($x2,1)];}}} // vetor resposta (equação, delta, x1, x2)
+        $this->valorC = $valorC;
+    }
+
+    private function escrever()
+    { // string da equação
+        if ($this->valorB < 0 && $this->valorC < 0) {
+            $eq = $this->valorA . "x<sup>2</sup>&nbsp;" . $this->valorB . "x&nbsp;" . $this->valorC . "&nbsp;=&nbsp;0";
+        } elseif ($this->valorB >= 0 && $this->valorC < 0) {
+            $eq = $this->valorA . "x<sup>2</sup>&nbsp;+" . $this->valorB . "x&nbsp;" . $this->valorC . "&nbsp;=&nbsp;0";
+        } elseif ($this->valorB < 0 && $this->valorC >= 0) {
+            $eq = $this->valorA . "x<sup>2</sup>&nbsp;" . $this->valorB . "x&nbsp;+" . $this->valorC . "&nbsp;=&nbsp;0";
+        } else {
+            $eq = $this->valorA . "x<sup>2</sup>&nbsp;+" . $this->valorB . "x&nbsp;+" . $this->valorC . "&nbsp;=&nbsp;0";
+        }
+        return $eq;
+    }
+
+    public function calcular()
+    { // cálculo da equação        
+        if ($this->valorA == 0) {
+            return ["Erro: 'A' não pode ser Zero!", '&empty;', '&empty;', '&empty;'];
+        } else {
+            $d = ((pow($this->valorB, 2)) - (4 * $this->valorA * $this->valorC)); // cálculo do delta               
+            if ($d < 0) {
+                return [$this->escrever(), number_format($d, 1), '&empty;', '&empty;'];
+            } else {
+                $x1 = ((-1 * $this->valorB) + (sqrt($d))) / (2 * $this->valorA); // cálculo de Basckara
+                $x2 = ((-1 * $this->valorB) - (sqrt($d))) / (2 * $this->valorA);
+                return [$this->escrever(), number_format($d, 1), number_format($x1, 1), number_format($x2, 1)];
+            }
+        }
+    } // vetor resposta (equação, delta, x1, x2)
 
 }
