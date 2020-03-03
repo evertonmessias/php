04146fe797777d51e0a487c670eed78e1b50d350
evertonmessias <everton.messias@gmail.com>
2020-03-03 13:05:12 -0300

    03-03-2020_13:05:12

--- sistemaoo/includes.php
+++ sistemaoo/includes.php
@@ -14,7 +14,8 @@ class Sistema
         }
     }
 
-    public function erro(){
+    public function erro()
+    {
         print "<div id='quadro'><div id='mensagem'></div></div>";
     }
 
@@ -23,7 +24,7 @@ class Sistema
         return new mysqli(servidor, usuario, senha, banco);
     }
 
-    public function consultar($tabela,$conexao)
+    public function consultar($tabela, $conexao)
     {
         $sql = "SELECT * from $tabela";
 
@@ -39,28 +40,19 @@ class Sistema
     {
         $total = 0;
         $id = 1;
-        if ($tipo == "alterar") {
-            $sql = "SELECT * from $tabela";
-            $lista = mysqli_query($conexao, $sql);
-            print "<table class='tabela'><tr><td colspan='2' class='tdida'><h4>ID</h4></td><td><h4>Nome</h4></td><td><h4>Telefone</h4></td><td><h4>E-Mail</h4></td></tr>";
-            while ($vetor = mysqli_fetch_array($lista)) {
-                print "<tr><td class='tdida'><input type='radio' name='id' id='id" . $id . "' value='" . $vetor[0] . "'/></td><td class='tdida'>" . $vetor[0] . "</td><td>" . $vetor[1] . "</td><td>" . $vetor[2] . "</td><td>" . $vetor[3] . "</td></tr>";
-                $id++;
-                $total++;
+        $sql = "SELECT * from $tabela";
+        $lista = $conexao->query($sql);
+        print "<table class='tabela'><tr><td colspan='2' class='tdida'><h4>ID</h4></td><td><h4>Nome</h4></td><td><h4>Telefone</h4></td><td><h4>E-Mail</h4></td></tr>";
+        foreach ($lista as $vetor) {
+            if ($tipo == "alterar") {
+                print "<tr><td class='tdida'><input type='radio' name='id' id='id" . $id . "' value='" . $vetor['id'] . "'/></td><td class='tdida'>" . $vetor['id'] . "</td><td>" . $vetor['nome'] . "</td><td>" . $vetor['telefone'] . "</td><td>" . $vetor['email'] . "</td></tr>";
+            } elseif ($tipo == "apagar") {
+                print "<tr><td class='tdida'><input type='checkbox' id='id" . $id . "' value='" . $vetor['id'] . "'/></td><td class='tdida'>" . $vetor['id'] . "</td><td>" . $vetor['nome'] . "</td><td>" . $vetor['telefone'] . "</td><td>" . $vetor['email'] . "</td></tr>";
             }
-            print "</table>";
-            print "<input type='hidden' id='total' value='" . $total . "'>";
-        } elseif ($tipo == "apagar") {
-            $sql = "SELECT * from $tabela";
-            $lista = mysqli_query($conexao, $sql);
-            print "<table class='tabela'><tr><td colspan='2' class='tdida'><h4>ID</h4></td><td><h4>Nome</h4></td><td><h4>Telefone</h4></td><td><h4>E-Mail</h4></td></tr>";
-            while ($vetor = mysqli_fetch_array($lista)) {
-                print "<tr><td class='tdida'><input type='checkbox' id='id" . $id . "' value='" . $vetor[0] . "'/></td><td class='tdida'>" . $vetor[0] . "</td><td>" . $vetor[1] . "</td><td>" . $vetor[2] . "</td><td>" . $vetor[3] . "</td></tr>";
-                $id++;
-                $total++;
-            }
-            print "</table>";
-            print "<input type='hidden' id='total' value='" . $total . "'>";
+            $id++;
+            $total++;
         }
+        print "</table>";
+        print "<input type='hidden' id='total' value='" . $total . "'>";
     }
 }
