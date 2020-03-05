e0c7836377d650f3db6d81b02a1695866fbaa07f
evertonmessias <everton.messias@gmail.com>
2020-03-04 21:59:38 -0300

    04-03-2020_21:59:38

--- composer/App/Model/Conexao.php
+++ composer/App/Model/Conexao.php
@@ -1,10 +1,7 @@
-
 <?php
 namespace App\Model;
-include '../../../config.php';
+include 'config.php';
 servidor('i');
-
-
 class Conexao
 {       // padrão DAO , data access object (separa lógica dos dados)
     private static $instance;
@@ -12,7 +9,7 @@ class Conexao
     {
         if (!isset(self::$instance)) {
             $dsn = "mysql:dbname=" . banco . ";host=" . servidor . "";
-            self::$instance = new PDO($dsn, usuario, senha);
+            self::$instance = new \PDO($dsn, usuario, senha);
         }
         return self::$instance;
     }
--- composer/App/Model/ProdutoDao.php
+++ composer/App/Model/ProdutoDao.php
@@ -2,9 +2,14 @@
 namespace App\Model;
 class ProdutoDao{
     public function create(Produto $p){
-        $sql = "INSERT INTO produtos (id,nome,descricao) VALUES (default,?,?)";
+        $sql = "INSERT INTO produtos VALUES (default,?,?)";
         $cadastrar = Conexao::getConn()->prepare($sql);
         $cadastrar->execute(array($p->getNome(),$p->getDescricao()));
+        if ($cadastrar){
+            echo "OK";
+        }else{
+            echo "BAD";
+        }
     }
     public function read(){
 
