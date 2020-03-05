bab282648246663757262c535cfa1f65bb458107
evertonmessias <everton.messias@gmail.com>
2020-03-05 16:53:49 -0300

    05-03-2020_16:53:49

--- sistemaoo/model/includes.php
+++ sistemaoo/model/includes.php
@@ -23,22 +23,8 @@ class Sistema
         return new mysqli(servidor, usuario, senha, banco);
     }
 
-    public function consultar($tabela, $conexao)
-    {
-        $sql = "SELECT * from $tabela";
-
-        $lista = $conexao->query($sql);
-        print "<table class='tabela'><tr><td class='tdid'><h4>ID</h4></td><td><h4>Nome</h4></td><td><h4>Telefone</h4></td><td><h4>E-Mail</h4></td></tr>";
-        foreach ($lista as $vetor) {
-            print "<tr><td class='tdid'>" . $vetor['id'] . "</td><td>" . $vetor['nome'] . "</td><td>" . $vetor['telefone'] . "</td><td>" . $vetor['email'] . "</td></tr>";
-        }
-        print "</table>";
-    }
-
     public function lista($tabela, $tipo, $conexao)
     {
-        $total = 0;
-        $id = 1;
         $sql = "SELECT * from $tabela";
         $lista = $conexao->query($sql);
         print "<table class='tabela'><tr><td class='tdida'><h4>ID</h4></td><td><h4>Nome</h4></td><td><h4>Telefone</h4></td><td><h4>E-Mail</h4></td></tr>";
@@ -59,11 +45,8 @@ class Sistema
                 <td>" . $vetor['telefone'] . "</td>
                 <td>" . $vetor['email'] . "</td>
                 </tr>";
-            }
-            $id++;
-            $total++;
+            }   
         }
-        print "</table>";
-        print "<input type='hidden' id='total' value='" . $total . "'>";
+        print "</table>";        
     }
 }
