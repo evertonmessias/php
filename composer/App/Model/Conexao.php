3fadc4f01cb00cd88c8e53640e1ed851b6034169
evertonmessias <everton.messias@gmail.com>
2020-03-04 23:43:01 -0300

    04-03-2020_23:43:01

--- composer/App/Model/Conexao.php
+++ composer/App/Model/Conexao.php
@@ -1,15 +1,14 @@
 <?php
 namespace App\Model;
-include 'Config.php';
-Servidor('i');
 class Conexao
 {       // padrão DAO , data access object (separa lógica dos dados)
     private static $instance;
     public static function getConn()
     {
+        $config = Servidor::serv('i');        
         if (!isset(self::$instance)) {
-            $dsn = "mysql:dbname=" . banco . ";host=" . servidor . "";
-            self::$instance = new \PDO($dsn, usuario, senha);
+            $dsn = "mysql:dbname=" . $config[3] . ";host=" . $config[0] . "";
+            self::$instance = new \PDO($dsn, $config[1], $config[2]);
         }
         return self::$instance;
     }
