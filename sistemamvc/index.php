1ff1e259c769c908acbc96a70e2fffa8e932c00d
evertonmessias <everton.messias@gmail.com>
2020-03-06 17:13:09 -0300

    06-03-2020_17:13:09

--- sistemamvc/index.php
+++ sistemamvc/index.php
@@ -1,15 +1,25 @@
 <?php
 include 'app/core/core.php';
 include 'app/controller/menu.php';
+include 'app/model/includes.php';
+include 'app/model/config.php';
+
 $head = file_get_contents('app/template/head.html');
 $nav_content = file_get_contents('app/template/nav_content.html');
 echo $head;
 
-ob_start(); // inicio captura do navegador
+ob_start(); // inicio captura do buffer navegador
 $core = new Core;
 $core->start($_GET);
 $saida = ob_get_contents();
-ob_end_clean(); // fim captura do navegador
+ob_end_clean(); // fim 
 
 $conteudo = str_replace('{{conteudo}}',$saida,$nav_content);
 echo $conteudo;
+
+if($conexao = Sistema::conexao()){
+    echo "OKKKK";
+}else{
+    echo "BADDD";
+}
+
