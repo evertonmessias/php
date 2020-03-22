c9f643c01f0b8850f9cff4bf54e338d6797349e9
evertonmessias <everton.messias@gmail.com>
2020-03-22 17:47:08 -0300

    22-03-2020_17:47:08

--- pag3.php
+++
@@ -1,5 +0,0 @@
-<p>pagina de contatos</p>
-<h1>CONTATOS</h1>
-<?php
-extract($_REQUEST);
-echo "<h3>$nome<br>$fone</h3>";
--- teste04.php
+++ teste04.php
@@ -1,16 +1,9 @@
-<?php
-$variaveis = array(
-"nome"=>"Éverton Messias",
-"fone"=>"+55 19 995947383"
-);
-
-echo "
 <html>
 <head>
 <title>IMC</title>
 <meta charset='UTF-8'/>
 <style type='text/css'>
-fieldset{display: block;position: relative;margin: 0 auto; width: 370px;height: 40px;}
+fieldset{display: block;position: relative;margin: 0 auto; width: 270px;height: 40px;}
 ul{list-style: none;position: relative;display: block;margin-left:-20px;}
 li{display: inline-block;position: relative;margin-left: 20px;}
 a{background-color: #808080;color: white;padding: 5px;text-decoration: none;}
@@ -26,18 +19,15 @@ section{display: block;position: relative;margin: 0 auto; width: 1000px;height: 
 <fieldset>
 
 <ul>
-
 <li><a href='?num=1'>Home</a></li>
 <li><a href='?num=2'>Fotos</a></li>
-<li><a href='?num=3&".http_build_query($variaveis)."'>Contatos</a></li>
-<li><a href='?num=4'>Redireciona</a></li>
-
+<li><a href='?num=3'>Outro</a></li>
 </ul>
 </fieldset>
 </form>
 <br><hr><br>
 <section>
-";
+<?php
 $pagina = @$_GET['num'];
 switch($pagina) {
 	case 1:
@@ -47,9 +37,6 @@ switch($pagina) {
 	include('./pag2.php');
 	break;
 	case 3:
-	include('./pag3.php');
-	break;
-	case 4:
 	header('Location:./teste05.php');
 	break;
 	default:
